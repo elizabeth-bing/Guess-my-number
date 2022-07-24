91a782aeadbb7ac1f55eb75ea2827c0e6931ab0a
Elizabeth Bing <elizabeth.bing@outlook.co.nz>
2022-07-24 20:20:28 +1200

    refactor code

--- script.js
+++ script.js
@@ -15,23 +15,28 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
 let highScore = 0
+const classNo = document.querySelector('.number')
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
-  console.log(guess, typeof guess)
+  // console.log(guess, typeof guess)
 
   //When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!'
+    displayMessage('No Number!')
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!'
-    document.querySelector('.number').textContent = secretNumber
+    displayMessage('Correct Number!')
+    classNo.textContent = secretNumber
 
     document.querySelector('body').style.backgroundColor = '#60b347'
 
-    document.querySelector('.number').style.width = '30rem'
+    classNo.style.width = '30rem'
 
     if (score > highScore) {
       highScore = score
@@ -41,12 +46,11 @@ document.querySelector('.check').addEventListener('click', function () {
     //When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? 'Too high!' : 'Too Low!'
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too Low!')
       score--
       document.querySelector('.score').textContent = score
     } else {
-      document.querySelector('.message').textContent = 'You lost the game :('
+      displayMessage('You lost the game :(')
       document.querySelector('.score').textContent = 0
     }
   }
@@ -54,11 +58,11 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20
   secretNumber = Math.trunc(Math.random() * 20) + 1
-  document.querySelector('.message').textContent = 'Start guessing...'
+  displayMessage('Start guessing...')
   document.querySelector('.score').textContent = score
-  document.querySelector('.number').textContent = '?'
+  classNo.textContent = '?'
   document.querySelector('.guess').value = '' //value of input is now empty
   document.querySelector('body').style.backgroundColor = '#222'
 
-  document.querySelector('.number').style.width = '15rem'
+  classNo.style.width = '15rem'
 })
